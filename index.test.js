9b42d31ac25772554591e24edd05f7b6e8ff3ae0
wmik <williemik.wmik@gmail.com>
2021-12-31 09:04:29 +0300

    🐞 fix: tests

--- index.test.js
+++ index.test.js
@@ -2,38 +2,37 @@ const test = require('ava');
 const { renderHook } = require('@testing-library/react-hooks');
 const useMediaRecorder = require('.');
 
-test('test', t => t.is(true));
-// test('hook options', t => {
-//   let { result } = renderHook(() => useMediaRecorder({}));
-//   let {
-//     error,
-//     isAudioMuted,
-//     liveStream,
-//     mediaBlob,
-//     status,
-//     getMediaStream,
-//     clearMediaStream,
-//     clearMediaBlob,
-//     muteAudio,
-//     unMuteAudio,
-//     startRecording,
-//     stopRecording,
-//     pauseRecording,
-//     resumeRecording
-//   } = result.current;
+test('hook options', t => {
+  let { result } = renderHook(() => useMediaRecorder({}));
+  let {
+    error,
+    isAudioMuted,
+    liveStream,
+    mediaBlob,
+    status,
+    getMediaStream,
+    clearMediaStream,
+    clearMediaBlob,
+    muteAudio,
+    unMuteAudio,
+    startRecording,
+    stopRecording,
+    pauseRecording,
+    resumeRecording
+  } = result.current;
 
-//   t.is(status, 'idle');
-//   t.is(error, null);
-//   t.is(liveStream, null);
-//   t.is(mediaBlob, null);
-//   t.false(isAudioMuted);
-//   t.true(typeof getMediaStream === 'function');
-//   t.true(typeof clearMediaStream === 'function');
-//   t.true(typeof clearMediaBlob === 'function');
-//   t.true(typeof muteAudio === 'function');
-//   t.true(typeof unMuteAudio === 'function');
-//   t.true(typeof startRecording === 'function');
-//   t.true(typeof stopRecording === 'function');
-//   t.true(typeof pauseRecording === 'function');
-//   t.true(typeof resumeRecording === 'function');
-// });
+  t.is(status, 'idle');
+  t.is(error, null);
+  t.is(liveStream, null);
+  t.is(mediaBlob, null);
+  t.false(isAudioMuted);
+  t.true(typeof getMediaStream === 'function');
+  t.true(typeof clearMediaStream === 'function');
+  t.true(typeof clearMediaBlob === 'function');
+  t.true(typeof muteAudio === 'function');
+  t.true(typeof unMuteAudio === 'function');
+  t.true(typeof startRecording === 'function');
+  t.true(typeof stopRecording === 'function');
+  t.true(typeof pauseRecording === 'function');
+  t.true(typeof resumeRecording === 'function');
+});
